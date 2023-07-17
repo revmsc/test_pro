313e14ea2f16b2ddd7e758fe69b870da4be1d464
revmsc <133049912+revmsc@users.noreply.github.com>
2023-07-17 17:37:15 +0530

    Update-new rev.py

--- rev.py
+++ rev.py
@@ -5,7 +5,7 @@ if amount >= 20000:
 elif amount >= 7000:
     interest=amount*7/100
     print("Your interest amount is" + " " + str(interest))
-elif amount>=5000:
+elif amount>=4000:
     interest = amount * 5 / 100
     print("Your interest amount is" + " " + str(interest))
 else:
